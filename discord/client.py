b9fa8467faedb14f0046b732393a0a0e62a784e5
tnt <45692264+tuna2134@users.noreply.github.com>
2021-11-10 19:06:43 +0900

    Update client.py

--- discord/client.py
+++ discord/client.py
@@ -2,8 +2,10 @@ from .http import HttpRequest
 
 class Client:
     def __init__(self, loop = None):
-        self.http = HttpRequest()
+        self.http = HttpRequest(self)
         self.loop = asyncio.get_event_loop() if loop is None else loop
+        self.cmd = []
+        self.cmd_data = []
         
     def dispatch(self, name, *args, **kwargs):
         try:
@@ -13,6 +15,26 @@ class Client:
         else:
             self.loop.create_task(coro)
             
-    def command(self, coro):
+    def event(self, coro):
         setattr(self, coro.__name__, coro)
         return coro
+    
+    def command(self, coro):
+        self.cmd.append(coro)
+        return coro
+    
+    async def fetch_command(self):
+        return await self.http.request("GET", f"/applications/{self.user.id}/commands")
+
+    async def set_command(self):
+        for coro in self.cmd:
+            cmd = {
+                "name": coro.__name__,
+                "type": 1,
+                "description": "..."
+            }
+            self.cmd_data.append(cmd)
+         
+    async def setup(self):
+        cmds = await self.fetch_command()
+        for data in cmds:
